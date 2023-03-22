5b27df714b75a56f89bbaf6914f009da2ee5d3c8
Yash Srivastava <85068689+yash-srivastava19@users.noreply.github.com>
2023-03-22 23:22:01 +0530

    Create eval_expr.py
    added eval_expr using sympy.sympify !!

---
+++ eval_expr.py
@@ -0,0 +1,28 @@
+## Add additional statistics here as well !!
+from sympy import sympify, SympifyError
+import numpy as np 
+
+def eval_expr(file_path:str):
+  with open(file_path) as fp:
+    file_list = np.loadtxt(fp, dtype=str)
+
+  test_kit = file_list[:100]
+  count_crct = 0
+  count_incrct = 0
+  for exp in test_kit:
+    try:
+      sympify(exp)
+      count_crct += 1
+    except (SympifyError, ZeroDivisionError) as error:
+      count_incrct += 1
+
+  tots = count_crct + count_incrct
+  return count_crct/tots, count_incrct/tots
+
+print('Random Distribution Statistics')
+corr, incorr = eval_expr('math.txt')
+print(f'Correct Probability : {} Incorrect Probability : {}')
+
+print('P-GLAm Distribution Statistics')
+corr, incorr = eval_expr('gen_text.txt')
+print(f'Correct Probability : {} Incorrect Probability : {}')
