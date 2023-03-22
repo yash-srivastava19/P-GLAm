5e5b736f0b523e6fde3bfb9b10ff8b350ccb8a2b
Yash Srivastava <85068689+yash-srivastava19@users.noreply.github.com>
2023-03-22 15:01:03 +0530

    Update gpt-2.py
    made beautiful

--- gpt-2.py
+++ gpt-2.py
@@ -26,18 +26,21 @@ trainData = data[N:]
 valData = data[:N]
 
 
-context_length = 10
-batch_size = 4     # how many independent sequences will we process in parallel
-eval_interval = 200
-torch.manual_seed(69)
-n_embd = 32
-dropout = 0.18
-n_layer = 3
-n_head = 3
-max_iters = 30
-eval_iters = 50
-learning_rate = 3e-4
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+@dataclass
+class TuningParameters:
+  context_length = 10  
+  batch_size = 8
+  eval_interval = 100
+  n_embd = 64
+  dropout = 0.2
+  n_layer = 15
+  n_head = 15
+  max_iters = 500
+  eval_iters = 50
+  learning_rate = 3e-4
+  device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+params = TuningParameters()
 
 
 @torch.no_grad()
@@ -45,9 +48,9 @@ def estimate_loss():
     """[Average loss across multiple batches ; Much more resistant to fluctuations.]"""
     out = {}
     model.eval()
-    for split in ['train', 'eval']:
-        losses = torch.zeros(eval_iters)
-        for k in range(eval_iters):
+    for split in ['train', 'val']:
+        losses = torch.zeros(params.eval_iters)
+        for k in range(params.eval_iters):
             X, Y = get_batch(split)
             logits, loss = model(X, Y)
             losses[k] = loss.item()
@@ -57,22 +60,86 @@ def estimate_loss():
 
 
 def get_batch(split):
+    """[Batch up the dataset into x and y. Make sure to do the computations on the device]"""
     data = trainData if split == 'train' else valData
-    ix = torch.randint(len(data) - context_length, (batch_size,))  # random offsets in the dataset.
-    x = torch.stack([data[i:i+context_length] for i in ix])
-    y = torch.stack([data[i+1: i+context_length+1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    ix = torch.randint(len(data) - params.context_length, (params.batch_size,))  # random offsets in the dataset.
+    x = torch.stack([data[i:i+params.context_length] for i in ix])
+    y = torch.stack([data[i+1: i+params.context_length+1] for i in ix])
+    x, y = x.to(params.device), y.to(params.device) 
     return x, y
 
 
 xb, yb = get_batch('train')
 
-for b in range(batch_size):
-    for t in range(context_length):
+for b in range(params.batch_size):
+    for t in range(params.context_length):
         context = xb[b, :t+1]
         target = yb[b, t]
 
+class SelfAttentionHead(nn.Module):
+    """[Can't thank Vasvani et. al. for this. It's all I need]"""
+    def __init__(self, head_size):
+        super().__init__()
+        self._key = nn.Linear(params.n_embd, head_size, bias=False)
+        self._query = nn.Linear(params.n_embd, head_size, bias=False)
+        self._value = nn.Linear(params.n_embd, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones(params.context_length, params.context_length)))
 
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self._key(x)  # (B, T, head_size)
+        q = self._query(x)  # (B, T, head_size)
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5  # divided by sqrt(head size) to control the variance.
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))  # TODO: Do not interact with future token.
+        wei = F.softmax(wei, dim=-1)  # TODO: This ensures that the distribution of tokens is normalized.
+
+        v = self._value(x)
+        out = wei @ v  # (B, T, T) @ (B, T, C) which gives (B, T, C) matrix !!
+        return out
+
+class MultiHeadAttention(nn.Module):
+    """ [Multiple Headed Self Attention in parallel] """
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([SelfAttentionHead(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size*num_heads, params.n_embd)
+        self.dropout = nn.Dropout(params.dropout) 
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim = -1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class FeedForward(nn.Module):
+    """ [A Linear Layer followed by a Relu and Dropout] """
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+            nn.Dropout(params.dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    """[Transformer block: communication followed by computation]"""
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd//n_head
+        self.sa = MultiHeadAttention(n_head, head_size)     # communication
+        self.ffwd = FeedForward(n_embd)                    # computation
+
+    def forward(self, x):
+        x = x+ self.sa(x)
+        x = x+  self.ffwd(x)
+        return x
+              
 class BigramLanguageModel(nn.module):
     def __init__(self, vocab_size):
         super().__init__()
@@ -111,84 +178,16 @@ class BigramLanguageModel(nn.module):
             idx = torch.cat((idx, idx_next), dim=1)   # from (B,T) to (B,T+1) in one iteration.
         return idx
 
-class SelfAttentionHead(nn.Module):
-    def __init__(self, head_size):
-        super().__init__()
-        self._key = nn.Linear(n_embd, head_size, bias=False)
-        self._query = nn.Linear(n_embd, head_size, bias=False)
-        self._value = nn.Linear(n_embd, head_size, bias=False)
-        self.register_buffer('tril', torch.tril(torch.ones(context_length, context_length)))
-
-    def forward(self, x):
-        B, T, C = x.shape
-        k = self._key(x)  # (B, T, head_size)
-        q = self._query(x)  # (B, T, head_size)
-
-        wei = q @ k.transpose(-2, -1) * C**-0.5  # divided by sqrt(head size) to control the variance.
-        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))  # TODO: Do not interact with future token.
-        wei = F.softmax(wei, dim=-1)  # TODO: This ensures that the distribution of tokens is normalized.
-
-        v = self._value(x)
-        out = wei @ v  # (B, T, T) @ (B, T, C) which gives (B, T, C) matrix !!
-        return out
-
-
-class MultiHeadAttention(nn.Module):
-    """ Multiple Scaled Self Attention in parallel """
-    def __init__(self, num_heads, head_size):
-        super().__init__()
-        self.heads = nn.ModuleList([SelfAttentionHead(head_size) for _ in range(num_heads)])
-        self.proj = nn.Linear(head_size*num_heads, n_embd)
-        self.dropout = nn.Dropout(dropout) 
-
-    def forward(self, x):
-        out = torch.cat([h(x) for h in self.heads], dim = -1)
-        out = self.dropout(self.proj(out))
-        return out
-    
-
-class FeedForward(nn.Module):
-    """ a simple linear layer followed by a non-linearity """
-
-    def __init__(self, n_embd):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(n_embd, 4 * n_embd),
-            nn.ReLU(),
-            nn.Linear(4 * n_embd, n_embd),
-            nn.Dropout(dropout),
-        )
-
-    def forward(self, x):
-        return self.net(x)
-    
-    
-
-class Block(nn.Module):
-    """[Transformer block: communication followed by computation]"""
-    def __init__(self, n_embd, n_head):
-        super().__init__()
-        head_size = n_embd//n_head
-        self.sa = MultiHeadAttention(n_head, head_size)     # communication
-        self.ffwd = FeedForward(n_embd)                    # computation
-
-    def forward(self, x):
-        x = x+ self.sa(x)
-        x = x+  self.ffwd(x)
-        return x
-
-class GPTLanguageModel(nn.Module):
+class PGLAm(nn.Module):
 
     def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
-        self.position_embedding_table = nn.Embedding(context_length, n_embd)
-        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
-        self.lm_head = nn.Linear(n_embd, vocab_size)
-
-        # better init, not covered in the original GPT video, but important, will cover in followup video
+        self.token_embedding_table = nn.Embedding(vocab_size, params.n_embd)
+        self.position_embedding_table = nn.Embedding(params.context_length, params.n_embd)
+        self.blocks = nn.Sequential(*[Block(params.n_embd, n_head=params.n_head) for _ in range(params.n_layer)])
+        self.ln_f = nn.LayerNorm(params.n_embd) # final layer norm
+        self.lm_head = nn.Linear(params.n_embd, vocab_size)
         self.apply(self._init_weights)
 
     def _init_weights(self, module):
@@ -204,7 +203,7 @@ class GPTLanguageModel(nn.Module):
 
         # idx and targets are both (B,T) tensor of integers
         tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=params.device)) # (T,C)
         x = tok_emb + pos_emb # (B,T,C)
         x = self.blocks(x) # (B,T,C)
         x = self.ln_f(x) # (B,T,C)
@@ -217,6 +216,7 @@ class GPTLanguageModel(nn.Module):
             logits = logits.view(B*T, C)
             targets = targets.view(B*T)
             loss = F.cross_entropy(logits, targets)
+            # perplexity = torch.exp(loss)
 
         return logits, loss
 
@@ -224,7 +224,7 @@ class GPTLanguageModel(nn.Module):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
             # crop idx to the last block_size tokens
-            idx_cond = idx[:, -context_length:]
+            idx_cond = idx[:, -params.context_length:]
             # get the predictions
             logits, loss = self(idx_cond)
             # focus only on the last time step
@@ -238,20 +238,19 @@ class GPTLanguageModel(nn.Module):
         return idx
     
     
-model = GPTLanguageModel()
+model = PGLAm()
 m = model.to(device)
-# print the number of parameters in the model
-print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
+print(f'{sum(p.numel() for p in m.parameters())/1e6:.2f} M parameters')
 
-# create a PyTorch optimizer
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.AdamW(model.parameters(), lr=params.learning_rate)
 
-for iter in range(max_iters):
+for iter in range(params.max_iters):
 
     # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0 or iter == max_iters - 1:
+    if iter % params.eval_interval == 0 or iter == params.max_iters - 1:
         losses = estimate_loss()
-        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        perplexity = torch.exp(loss)
+        print(f"Step: {iter}, Train Loss: {losses['train']:.4f}, Val Loss: {losses['val']:.4f}, Perplexity: {perplexity.item()}")
 
     # sample a batch of data
     xb, yb = get_batch('train')
@@ -263,6 +262,6 @@ for iter in range(max_iters):
     optimizer.step()
 
 # generate from the model
-context = torch.zeros((1, 1), dtype=torch.long, device=device)
+context = torch.zeros((1, 1), dtype=torch.long, device=params.device)
 print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
-#open('more.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))    
+#open('gen_text.txt', 'w').write(decode(m.generate(context, max_new_tokens=10000)[0].tolist()))    
